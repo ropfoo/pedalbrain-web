4b3b46bd9af82d541513b139d4530cac214c9941
RopFoo <ropfoo@gmail.com>
2022-08-15 19:21:31 +0200

    pedal list styling

--- app/components/PedalList/PedalList.tsx
+++ app/components/PedalList/PedalList.tsx
@@ -9,7 +9,7 @@ export default function PedalList({ pedals }: PedalListProps) {
   if (!pedals) return <p>no pedals</p>;
 
   return (
-    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-x-10">
+    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-x-16">
       {pedals.map((pedal) => (
         <PedalListItem key={pedal?.id} pedal={pedal} />
       ))}
